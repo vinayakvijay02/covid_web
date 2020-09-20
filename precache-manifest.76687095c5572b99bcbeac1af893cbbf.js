b928a87cd0811f78363dc78d124836f5d9877b01
Vinayak Nadar <vinu142@gmail.com>
2020-09-20 18:57:51 +1000

    Updates

---
+++ precache-manifest.76687095c5572b99bcbeac1af893cbbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3899a08bbdf0cdfc7e2522664d40f0b0",
+    "url": "/covid_web/index.html"
+  },
+  {
+    "revision": "5ad46cd6cbe289f42bb6",
+    "url": "/covid_web/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "02ba65e5377b43c014fd",
+    "url": "/covid_web/static/css/main.8fcf1444.chunk.css"
+  },
+  {
+    "revision": "5ad46cd6cbe289f42bb6",
+    "url": "/covid_web/static/js/2.9d04af84.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid_web/static/js/2.9d04af84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02ba65e5377b43c014fd",
+    "url": "/covid_web/static/js/main.4b5d8fe3.chunk.js"
+  },
+  {
+    "revision": "54b9946d84df5bb1c365",
+    "url": "/covid_web/static/js/runtime-main.35c11804.js"
+  }
+]);
